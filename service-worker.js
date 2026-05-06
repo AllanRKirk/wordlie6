5d7650d04fa688727fbd8bd549b9f0ff97a5cd9a
AllanRKirk <66125681+AllanRKirk@users.noreply.github.com>
2026-05-06 16:14:43 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,22 @@
-// JavaScript source code
-const CACHE_NAME = "wordlie6-cache-v1";
-const ASSETS = [
-  "wordlie6.html",
-  "wordlie6.css",
-  "wordlie6.js",
-  "dictionary.js",
-  "manifest.json",
-  "assets/images/wordlie6.jpg",
-  "assets/sounds/cheers.mp3"
-];
-
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("wordlie6-cache").then(cache => {
+      return cache.addAll([
+        "./",
+        "wordlie6.html",
+        "wordlie6.css",
+        "wordlie6.js",
+        "dictionary.js",
+        "manifest.json"
+      ]);
+    })
+  );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
