a7b0e08399f57ab5213a3d13355348ea1b596169
AllanRKirk <66125681+AllanRKirk@users.noreply.github.com>
2026-05-15 15:25:09 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -49,13 +49,8 @@ self.addEventListener("install", event => {
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      );
-    })
-  );
   clients.claim(); // NEW: take control of all pages immediately
+);
 });
 
 self.addEventListener("fetch", event => {
